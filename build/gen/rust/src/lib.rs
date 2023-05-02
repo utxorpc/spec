762325e078d5b07fa44bfbd8897def28c7b1ce02
Santiago Carmuega <santiago@carmuega.me>
2023-05-02 04:06:59 +0200

    fix(build): Use correct package name in proto (#10)

--- build/gen/rust/src/lib.rs
+++ build/gen/rust/src/lib.rs
@@ -1,10 +1,10 @@
 // @generated
 pub mod utxorpc {
-    pub mod state {
-        // @@protoc_insertion_point(attribute:utxorpc.state.v1)
+    pub mod build {
+        // @@protoc_insertion_point(attribute:utxorpc.build.v1)
         pub mod v1 {
-            include!("utxorpc.state.v1.rs");
-            // @@protoc_insertion_point(utxorpc.state.v1)
+            include!("utxorpc.build.v1.rs");
+            // @@protoc_insertion_point(utxorpc.build.v1)
         }
     }
 }
